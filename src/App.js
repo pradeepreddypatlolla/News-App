337266dab8ce42de9c127cb8e8c0c844e43fb7a5
pradeep <pradeepreddyp1997@gmail.com>
2022-08-19 23:13:00 +0530

    NewsApp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import { useState } from 'react';
-import './App.css';
+
+
 
 import NewsApp from './NewsApp/NewsApp';
-import QuizApp from './QuizApp/QuizApp';
+
 function App() {
-  const [appOption,setAppOption]=useState("NewsApp")
-  const switchApp=()=>{
-    switch (appOption) {
-      case "NewsApp":
-        return <NewsApp/>
-        
-    case "QuizApp":
-      return <QuizApp/>
-     
-      default:
-        break;
-    }
-  }
+  
+  
   return (
     <div className="App">  
-
-    <select name="" id="" onChange={(e)=>setAppOption(e.target.value)} > 
-        <option value="NewsApp" > News App </option>
-        <option value="QuizApp">Quiz App</option>
-        
-     </select>
-   
-   {switchApp()}
+      <NewsApp/>
     </div>
     
   );
